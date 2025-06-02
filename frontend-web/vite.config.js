642479841c5c82789f1ca3552f25875fa979d429
Abdulhadi alayoub <115499325+Abdulhadialayoub@users.noreply.github.com>
2025-06-02 07:04:04 +0300

    Update vite.config.js

--- frontend-web/vite.config.js
+++ frontend-web/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5105',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+  base: process.env.VITE_BASE_PATH || '/TrackMate',
+});
